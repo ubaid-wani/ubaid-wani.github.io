22696ef14480603afb7adfa0bc0321160b10c6eb
ubaid-wani <ubaidwani133@gmail.com>
2024-04-27 11:50:17 +0530

    new images added

--- src/data/data.js
+++ src/data/data.js
@@ -1,35 +1,32 @@
-import WorkIm from '../assets/projects/workImg.jpeg'
-import RealEst from '../assets/projects/realestate.jpg'
-import portfolio from '../assets/projects/portfolio.jpg'
-import todoapp from '../assets/projects/todoapp.jpg'
-import cryptotracker from '../assets/projects/cryptotracker.jpg'
-import weatherapp from '../assets/projects/weatherapp.jpg'
+import img1 from '../assets/projects/img1.jpg'
+import img2 from '../assets/projects/img2.jpg'
+import img3 from '../assets/projects/img3.png'
 export const data=[
     {
         id:1,
         name:"React-Crypto-Tracker",
-        image:cryptotracker,
+        image:img3,
         github:"",
         live:"https://ubaid-wani.github.io/cryptotracker-react/",
     },
     {
         id:2,
         name:"React-TodoApp",
-        image:todoapp,
+        image:img2,
         github:"",
         live:"https://ubaid-wani.github.io/todoapp-react/",
     },
     {
         id:3,
         name:"Weather App",
-        image:weatherapp,
+        image:img1,
         github:"",
         live:"https://ubaidwaniweather.netlify.app/",
     },
     {
         id:4,
         name:"Portfolio website ",
-        image:portfolio,
+        image:img3,
         github:"",
         live:"https://portfolioubaidwani.netlify.app/",
     },
