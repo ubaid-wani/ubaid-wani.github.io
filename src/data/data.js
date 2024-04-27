d4a453671b44e2ae5123c61766cab2b484c3396c
ubaid-wani <ubaidwani133@gmail.com>
2024-04-27 12:22:18 +0530

    cal proj added

--- src/data/data.js
+++ src/data/data.js
@@ -1,6 +1,7 @@
 import img1 from '../assets/projects/img1.jpg'
 import img2 from '../assets/projects/img2.jpg'
 import img3 from '../assets/projects/img3.png'
+import cal from '../assets/projects/cal.png'
 export const data=[
     {
         id:1,
@@ -30,6 +31,14 @@ export const data=[
         github:"",
         live:"https://portfolioubaidwani.netlify.app/",
     },
+    {
+        id:5,
+        name:"Calculator ",
+        image:cal,
+        github:"",
+        live:"https://ubaid-wani.github.io/calculator-js/",
+    },
+
 
 
 ]
